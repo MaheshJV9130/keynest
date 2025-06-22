9cc237e1a90ebf00881a7ffb2ab2a121064eb180
MaheshJV9130 <maheshvispute082@gmail.com>
2025-06-22 14:31:23 +0530

    inial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav className="bg-slate-800">
+        <div className="logo">
+          <img
+            src="/src/assets/logo/logo-transparent-png.png"
+            alt="logo"
+            width={150}
+          />
+        </div>
+        <ol>
+          <li>
+            <a href=""></a>
+          </li>
+        </ol>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
